455dbaecc55c932aa67ab6683f91de964dd64f1f
TucumánCompras <32079789+TucumanCompras@users.noreply.github.com>
2017-10-18 20:45:41 -0300

    Update pre-instalacion.sh

--- pre-instalacion.sh
+++ pre-instalacion.sh
@@ -1,15 +1,16 @@
 # Asignacion de permisos iniciales
-sudo find . -type f -exec chmod 644 {} \;
-sudo find . -type d -exec chmod 755 {} \;
-sudo find ./var -type d -exec chmod 777 {} \;
-sudo find ./app/etc -type f -exec chmod 777 {} \;
-sudo find ./var -type f -exec chmod 777 {} \;
-sudo find ./pub/media -type f -exec chmod 777 {} \;
-sudo find ./pub/static -type f -exec chmod 777 {} \;
-sudo find ./generated -type f -exec chmod 777 {} \;
-sudo find ./pub/media -type d -exec chmod 777 {} \;
-sudo find ./pub/static -type d -exec chmod 777 {} \;
-sudo chmod 777 ./app/etc
-sudo chmod 644 ./app/etc/*.xml
-sudo chown -R :daemon .
-sudo chmod u+x bin/magento
+DIRBASE=$(dirname $0)
+sudo find $DIRBASE -type f -exec chmod 644 {} \;
+sudo find $DIRBASE -type d -exec chmod 755 {} \;
+sudo find $DIRBASE/var -type d -exec chmod 777 {} \;
+sudo find $DIRBASE/app/etc -type f -exec chmod 777 {} \;
+sudo find $DIRBASE/var -type f -exec chmod 777 {} \;
+sudo find $DIRBASE/pub/media -type f -exec chmod 777 {} \;
+sudo find $DIRBASE/pub/static -type f -exec chmod 777 {} \;
+sudo find $DIRBASE/generated -type f -exec chmod 777 {} \;
+sudo find $DIRBASE/pub/media -type d -exec chmod 777 {} \;
+sudo find $DIRBASE/pub/static -type d -exec chmod 777 {} \;
+sudo chmod 777 $DIRBASE/app/etc
+sudo chmod 644 $DIRBASE/app/etc/*.xml
+sudo chown -R :daemon $DIRBASE
+sudo chmod u+x $DIRBASE/bin/magento
