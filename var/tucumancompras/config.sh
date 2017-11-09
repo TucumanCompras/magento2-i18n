bf69e444982deab671a2c975e1576f4f45c8e4b2
TucumánCompras <32079789+TucumanCompras@users.noreply.github.com>
2017-11-09 16:24:57 -0300

    Create config.sh

---
+++ var/tucumancompras/config.sh
@@ -0,0 +1,13 @@
+#VARIABLES GENERALES
+EMAILADMIN=info@tucumancompras.com.ar
+
+#RUTAS Y ARCHIVOS
+URLBASE=dev.sporting.com.ar
+RUTABASE=/opt/bitnami/apache2/htdocs
+RUTABACKUP=/datos/backups/tucumancompras.com.ar
+
+#BD INFO
+BDHOST=localhost
+BDUSER=root
+BDPASS=YzFN6EB1Bk5U
+BDNAME=magento
