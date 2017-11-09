09ea0182b36db2b14126595319584adeca236e6e
TucumánCompras <32079789+TucumanCompras@users.noreply.github.com>
2017-11-09 16:32:43 -0300

    Update magento_deploy.sh

--- var/tucumancompras/magento_deploy.sh
+++ var/tucumancompras/magento_deploy.sh
@@ -1,24 +1,26 @@
-#echo "--> Magento init <-----------------------------------------------------------------|||||||||||||||||||||"
-#sudo /opt/bitnami/apps/magento/htdocs/var/tucumancompras/shell/magento_init.sh
+#!/bin/sh
+
+#Incluyo la configuracion
+. "$(dirname "$0")"/config.sh
 
 echo "--> borrar cache, generation, di <-------------------------------------------------|||||||||||||||||||||"
-sudo rm -R /opt/bitnami/apps/magento/htdocs/var/cache/*
-sudo rm -R /opt/bitnami/apps/magento/htdocs/var/generation/*
-sudo rm -R /opt/bitnami/apps/magento/htdocs/var/di/*
-sudo rm -R /opt/bitnami/apps/magento/htdocs/var/page_cache/*
+sudo rm -R $RUTABASE/var/cache/*
+sudo rm -R $RUTABASE/var/generation/*
+sudo rm -R $RUTABASE/var/di/*
+sudo rm -R $RUTABASE/var/page_cache/*
 
 echo "--> upgrade, compile, deploy <-----------------------------------------------------|||||||||||||||||||||"
-sudo php /opt/bitnami/apps/magento/htdocs/bin/magento setup:upgrade
-sudo php /opt/bitnami/apps/magento/htdocs/bin/magento setup:di:compile
-sudo php /opt/bitnami/apps/magento/htdocs/bin/magento setup:static-content:deploy es_ES
+sudo php $RUTABASE/bin/magento setup:upgrade
+sudo php $RUTABASE/bin/magento setup:di:compile
+sudo php $RUTABASE/bin/magento setup:static-content:deploy es_ES
 
 echo "--> reindex, clean y flush cache <-------------------------------------------------|||||||||||||||||||||"
-sudo php /opt/bitnami/apps/magento/htdocs/bin/magento indexer:reindex
-sudo php /opt/bitnami/apps/magento/htdocs/bin/magento cache:clean
-sudo php /opt/bitnami/apps/magento/htdocs/bin/magento cache:flush
+sudo php $RUTABASE/bin/magento indexer:reindex
+sudo php $RUTABASE/bin/magento cache:clean
+sudo php $RUTABASE/bin/magento cache:flush
 
 echo "--> reiniciando servicios <--------------------------------------------------------|||||||||||||||||||||"
 sudo /opt/bitnami/ctlscript.sh restart
 
 echo "--> Magento init <-----------------------------------------------------------------|||||||||||||||||||||"
-sudo /opt/bitnami/apps/magento/htdocs/var/tucumancompras/shell/magento_init.sh
+sudo $RUTABASE/var/tucumancompras/shell/magento_init.sh
