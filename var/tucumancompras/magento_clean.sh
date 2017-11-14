ff2a687c00ca39e24e9a4ffa3e964496ad7bea35
TucumánCompras <32079789+TucumanCompras@users.noreply.github.com>
2017-11-14 09:38:14 -0300

    Update magento_clean.sh

--- var/tucumancompras/magento_clean.sh
+++ var/tucumancompras/magento_clean.sh
@@ -6,17 +6,11 @@
 sudo chown -R bitnami:daemon $RUTABASE
 
 sudo rm -R $RUTABASE/var/cache/*
-#sudo rm -R $RUTABASE/var/generation/*
-#sudo rm -R $RUTABASE/var/di/*
 sudo rm -R $RUTABASE/var/page_cache/*
 
-#sudo php $RUTABASE/bin/magento setup:upgrade
-#sudo php $RUTABASE/bin/magento setup:di:compile
-#sudo php $RUTABASE/bin/magento setup:static-content:deploy
 sudo php $RUTABASE/bin/magento indexer:reindex
 sudo php $RUTABASE/bin/magento cache:clean
+sudo php $RUTABASE/bin/magento cache:flush
 
 sudo chown -R bitnami:daemon $RUTABASE/var/cache
 sudo chmod -R 777 $RUTABASE/var/cache
-
-#sudo /opt/bitnami/ctlscript.sh restart
