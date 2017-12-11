6de7907e7497208752fdba2612aa5567d64d0914
TucumánCompras <32079789+TucumanCompras@users.noreply.github.com>
2017-12-11 16:31:25 -0300

    Update magento_init.sh

--- var/tucumancompras/shell/magento_init.sh
+++ var/tucumancompras/shell/magento_init.sh
@@ -4,16 +4,8 @@
 . "$(dirname "$0")"/config.sh
 
 # Actualizo las rutas de MySQL
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="http://'$URLBASE'/" WHERE path="web/unsecure/base_url" AND scope_id=0' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="https://'$URLBASE'/" WHERE path="web/secure/base_url" AND  scope_id=0' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="http://agropac.tucumancompras.com.ar/" WHERE path="web/unsecure/base_url" AND scope_id=2' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="https://agropac.tucumancompras.com.ar/" WHERE path="web/secure/base_url" AND  scope_id=2' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="http://www.solmar.com.ar/" WHERE path="web/unsecure/base_url" AND scope_id=3' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="https://www.solmar.com.ar/" WHERE path="web/secure/base_url" AND  scope_id=3' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="http://whitebox.tucumancompras.com.ar/" WHERE path="web/unsecure/base_url" AND scope_id=4' $BDNAME
-#mysql -u$BDUSER -p$BDPASS -e 'UPDATE core_config_data SET value="https://whitebox.tucumancompras.com.ar/" WHERE path="web/secure/base_url" AND  scope_id=4' $BDNAME
 
-#mysql -u$BDUSER -p$BDPASS -e 'delete from core_config_data where path like "%base%" and scope <> "default";'
+mysql -u$BDUSER -p$BDPASS -e 'delete from core_config_data where path like "%base%" and scope <> "default";'
 
 sudo -u bitnami n98-magerun2.phar config:store:set --scope default --scope-id 0 web/unsecure/base_url http://dev.tucumancompras.com.ar/
 sudo -u bitnami n98-magerun2.phar config:store:set --scope default --scope-id 0 web/secure/base_url https://dev.tucumancompras.com.ar/
