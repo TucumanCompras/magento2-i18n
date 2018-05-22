05678dc41f6e37056438441d9ba1be5275a76194
TucumánCompras <32079789+TucumanCompras@users.noreply.github.com>
2018-05-22 11:16:08 -0300

    Update git_clone.sh

--- var/tucumancompras/shell/git_clone.sh
+++ var/tucumancompras/shell/git_clone.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 #Incluyo la configuracion
-. "$(dirname "$0")"/config.sh
+RUTABASE=/home/momitoys/public_html
 
-if [ -d "$RUTABASE/temporal" ]; then rm -Rf $RUTABASE/temporal; fi
-#rm -R $RUTABASE/temporal
-mkdir -p $RUTABASE/temporal
-git clone https://github.com/TucumanCompras/magento2.git $RUTABASE/temporal
-cp -R $RUTABASE/temporal/* $RUTABASE
-if [ -d "$RUTABASE/temporal" ]; then rm -Rf $RUTABASE/temporal; fi
-chmod -R 777 $RUTABASE/var/tucumancompras/shell/*.sh
+if [ -d "$RUTABASE/temporal" ]; then rm -Rf $RUTABASE/temporal_git; fi
+#rm -R $RUTABASE/temporal_git
+mkdir -p $RUTABASE/temporal_git
+git clone https://github.com/TucumanCompras/magento2-i18n.git $RUTABASE/temporal_git
+cp -R $RUTABASE/temporal_git/* $RUTABASE
+if [ -d "$RUTABASE/temporal_git" ]; then rm -Rf $RUTABASE/temporal_git; fi
